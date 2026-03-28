16e9b4e2f568baca19ff013726b3d4c0d0eed8e6
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-03-28 12:55:29 +0800

    Add solution to reorder singly-linked list
    Implement a solution to reorder a singly-linked list in a specific pattern.

---
+++ ListNode/143. 重排链表.cpp
@@ -0,0 +1,83 @@
+/*
+给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+
+L0 → L1 → … → Ln - 1 → Ln
+请将其重新排列后变为：
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例 1：
+
+
+
+输入：head = [1,2,3,4]
+输出：[1,4,2,3]
+示例 2：
+
+
+
+输入：head = [1,2,3,4,5]
+输出：[1,5,2,4,3]
+ 
+
+提示：
+
+链表的长度范围为 [1, 5 * 104]
+1 <= node.val <= 1000
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode *mid = middleNode(head);
+        ListNode *l2 = mid->next;
+        mid->next = nullptr;
+        l2 = reverseNode(l2);
+        mergeList(head, l2);
+    }
+
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* reverseNode(ListNode* head) {
+        ListNode *cur = head, *pre = nullptr;
+        while(cur) {
+            ListNode *tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    void mergeList(ListNode* l1, ListNode* l2) {
+        ListNode *l1_tmp, *l2_tmp;
+        while(l1 && l2) {
+            l1_tmp = l1->next;
+            l2_tmp = l2->next;
+            l1->next = l2;
+            l1 = l1_tmp;
+            l2->next = l1;
+            l2 = l2_tmp;
+        }
+    }
+};
