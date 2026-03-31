5dc58c7ab1e8df601d1f6725c67c99530db700e5
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-03-31 23:44:01 +0800

    Add inorder traversal for binary tree
    Implement inorder traversal for a binary tree using both recursive and iterative methods.

---
+++ Binary Tree/94. 二叉树的中序遍历.cpp
@@ -0,0 +1,88 @@
+/*
+给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+*/
+
+// 1、递归
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode *cur, vector<int> &result)
+    {
+        if(cur == nullptr) return;
+
+        traversal(cur->left, result);
+        result.push_back(cur->val);
+        traversal(cur->right, result);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
+
+// 2、迭代
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while(cur || !st.empty()) {
+            if(cur) {
+                st.push(cur);
+                cur = cur->left;
+            } else {
+                cur = st.top();
+                st.pop();
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return result;
+    }
+};
