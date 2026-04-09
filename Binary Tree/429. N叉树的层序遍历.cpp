449c45afcdeaec68cfafa32295953817764ec4ec
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-04-09 15:33:34 +0800

    Implement level order traversal for N-ary tree

---
+++ Binary Tree/429. N叉树的层序遍历.cpp
@@ -0,0 +1,69 @@
+/*
+给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+
+树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+
+ 
+
+示例 1：
+
+
+
+输入：root = [1,null,3,2,4,null,5,6]
+输出：[[1],[3,2,4],[5,6]]
+示例 2：
+
+
+
+输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+ 
+
+提示：
+
+树的高度不会超过 1000
+树的节点总数在 [0, 104] 之间
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        queue<Node*> que;
+        if(root) que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            vector<int> nums;
+            for(int i = 0; i < size; i++) {
+                Node *node = que.front();
+                que.pop();
+                nums.push_back(node->val);
+                for(int i = 0; i < node->children.size(); i++) {
+                    if(node->children[i]) que.push(node->children[i]);
+                }
+            }
+            result.push_back(nums);
+        }
+        return result;
+    }
+};
