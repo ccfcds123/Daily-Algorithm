7a9d4a0061f4c972d302c01ab354e7ca6a40540b
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-04-11 16:58:45 +0800

    Add minDepth function for binary tree
    Implement a function to find the minimum depth of a binary tree.

---
+++ Binary Tree/111. 二叉树的最小深度.cpp
@@ -0,0 +1,63 @@
+/*
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+示例 2：
+
+输入：root = [2,null,3,null,4,null,5,null,6]
+输出：5
+ 
+
+提示：
+
+树中节点数的范围在 [0, 105] 内
+-1000 <= Node.val <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        int depth = 0, mindepth = 0;
+        queue<TreeNode*> que;
+        if(!root) return mindepth;
+        que.push(root);
+        bool flag = true;
+        while(!que.empty() && flag) {
+            int size = que.size();
+            depth++;
+            for(int i = 0; i < size; i++) {
+                TreeNode *node = que.front();
+                que.pop();
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+                if(!node->left && !node->right) {
+                    mindepth = depth;
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return mindepth;
+    }
+};
