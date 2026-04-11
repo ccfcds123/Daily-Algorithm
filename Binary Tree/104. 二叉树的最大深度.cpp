3f397c9c44507094e946de83a8faa991889d3734
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-04-11 16:57:53 +0800

    Add maxDepth function for binary tree
    Implement a function to calculate the maximum depth of a binary tree using breadth-first search.

---
+++ Binary Tree/104. 二叉树的最大深度.cpp
@@ -0,0 +1,57 @@
+/*
+给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+ 
+
+示例 1：
+
+
+
+ 
+
+输入：root = [3,9,20,null,null,15,7]
+输出：3
+示例 2：
+
+输入：root = [1,null,2]
+输出：2
+ 
+
+提示：
+
+树中节点的数量在 [0, 104] 区间内。
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        queue<TreeNode*> que;
+        if(root) que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode *node = que.front();
+                que.pop();
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
