12ae107e06f4a5571ee9f9e20175177434c127db
ccfcds123 <142216053+ccfcds123@users.noreply.github.com>
2026-04-14 20:24:24 +0800

    Add solution for binary tree diameter problem
    Implement the diameter calculation for a binary tree.

---
+++ Binary Tree/543. 二叉树的直径.cpp
@@ -0,0 +1,55 @@
+/*
+给你一棵二叉树的根节点，返回该树的 直径 。
+
+二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+
+两节点之间路径的 长度 由它们之间边数表示。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,2,3,4,5]
+输出：3
+解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+示例 2：
+
+输入：root = [1,2]
+输出：1
+ 
+
+提示：
+
+树中节点数目在范围 [1, 104] 内
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int ans;
+public:
+    int depth(TreeNode* root) {
+        if(!root) return 0;
+        int l = depth(root->left);
+        int r = depth(root->right);
+        ans = max(ans, l + r + 1);
+        return max(l, r) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+};
